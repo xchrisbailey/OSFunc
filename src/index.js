8d28f64ee4853e65cc573b57e8e399ba18ab2928
Chris Bailey <chris@ghostly.io>
2021-12-07 19:44:43 -0500

    log file adjustments

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const fs = require("fs");
+const path = require("path");
 
-const logFile = fs.createWriteStream("./logs/info.log", { flags: "a" }); //use {flags: 'w'} to open in write mode
-const pino = require("pino")(logFile);
+const logFile = fs.createWriteStream(path.join(__dirname, "info.log"), {
+  flags: "a",
+});
+const pino = require("pino")({ stream: logFile });
 
 const expressPino = require("express-pino-logger")({
   logger: pino,
