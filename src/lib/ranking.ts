4f937fcd12d932af7067952079cafd16844c6b1e
Chris Bailey <chris@ghostly.io>
2021-12-08 00:24:48 -0500

    trying to best cf

--- src/lib/ranking.ts
+++ src/lib/ranking.ts
@@ -1,5 +1,6 @@
 import puppeteer from "puppeteer-extra";
 import StealthPlugin from "puppeteer-extra-plugin-stealth";
+import randomUserAgent from "random-useragent";
 import { Collection } from "../types";
 
 const os_url = "https://opensea.io/rankings";
@@ -21,10 +22,77 @@ export const getRanks = async (): Promise<Collection[]> => {
   try {
     const page = await browser.newPage();
     await page.setUserAgent(
-      "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36"
+      randomUserAgent.getRandom({
+        deviceCategory: "desktop",
+        os: "Windows",
+      })
     );
 
-    await page.setViewport({ width: 1900, height: 1200 });
+    await page.setViewport({
+      width: 1920 + Math.floor(Math.random() * 100),
+      height: 3000 + Math.floor(Math.random() * 100),
+      deviceScaleFactor: 1,
+      hasTouch: false,
+      isLandscape: false,
+      isMobile: false,
+    });
+
+    await page.setJavaScriptEnabled(true);
+    page.setDefaultNavigationTimeout(0);
+
+    await page.setRequestInterception(true);
+    page.on("request", (req) => {
+      if (
+        req.resourceType() == "stylesheet" ||
+        req.resourceType() == "font" ||
+        req.resourceType() == "image"
+      ) {
+        req.abort();
+      } else {
+        req.continue();
+      }
+    });
+
+    await page.evaluateOnNewDocument(() => {
+      //pass webdriver check
+      Object.defineProperty(navigator, "webdriver", {
+        get: () => false,
+      });
+    });
+
+    await page.evaluateOnNewDocument(() => {
+      //pass chrome check
+      window.chrome = {
+        runtime: {},
+        // etc.
+      };
+    });
+
+    await page.evaluateOnNewDocument(() => {
+      //pass plugins check
+      const originalQuery = window.navigator.permissions.query;
+      return (window.navigator.permissions.query = (parameters) =>
+        parameters.name === "notifications"
+          ? Promise.resolve({ state: Notification.permission })
+          : originalQuery(parameters));
+    });
+
+    await page.evaluateOnNewDocument(() => {
+      // Overwrite the `plugins` property to use a custom getter.
+      Object.defineProperty(navigator, "plugins", {
+        // This just needs to have `length > 0` for the current test,
+        // but we could mock the plugins too if necessary.
+        get: () => [1, 2, 3, 4, 5],
+      });
+    });
+
+    await page.evaluateOnNewDocument(() => {
+      // Overwrite the `plugins` property to use a custom getter.
+      Object.defineProperty(navigator, "languages", {
+        get: () => ["en-US", "en"],
+      });
+    });
+
     await page.goto(os_url, { waitUntil: "domcontentloaded" });
     // await page.waitForSelector("[role='row']");
     console.log(
