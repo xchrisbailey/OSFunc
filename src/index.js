acf0ace5bd94d9361f3185fc4d5da554cd13f454
Chris Bailey <chris@ghostly.io>
2021-12-07 19:30:38 -0500

    logging

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 const express = require("express");
+const pino = require("pino")("./logs/info.log");
+const expressPino = require("express-pino-logger")({
+  logger: pino,
+});
+
 const getRanks = require("./lib/ranking");
+
 const app = express();
 
+app.use(expressPino);
 app.set("port", process.env.PORT || 4000);
 
 app.get("/rank", async (_req, res) => {
