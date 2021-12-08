07a1914a5e3f0ef13dcce6281976acf83d2be764
Chris Bailey <chris@ghostly.io>
2021-12-07 19:51:45 -0500

    remove logging for now

--- src/index.js
+++ src/index.js
@@ -1,21 +1,9 @@
 const express = require("express");
-const fs = require("fs");
-const path = require("path");
-
-const logFile = fs.createWriteStream(path.join(__dirname, "info.log"), {
-  flags: "a",
-});
-const pino = require("pino")({ stream: logFile });
-
-const expressPino = require("express-pino-logger")({
-  logger: pino,
-});
 
 const getRanks = require("./lib/ranking");
 
 const app = express();
 
-app.use(expressPino);
 app.set("port", process.env.PORT || 4000);
 
 app.get("/rank", async (_req, res) => {
